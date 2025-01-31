e44bae8d85c44c35f1c8a32a90631d5231c04f61
Ilya <simonewarlet@gmail.com>
2025-01-31 13:57:43 +0200

    Add routes for chat

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@
 
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
   }
-  root 'home#index'
+  root 'rooms#index'
+
+  resources :users
+  resources :rooms do
+    resources :messages
+  end
+
   get 'up' => 'rails/health#show', as: :rails_health_check
 end
