fa238b0e2474026cea128179aadbc7ca434a354b
Ilya <simonewarlet@gmail.com>
2025-01-31 13:56:32 +0200

    Add validations and dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :username, presence: true, length: { maximum: 20 }
+  validates :email, presence: true, length: { maximum: 255 }
+  validates :first_name, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :last_name, presence: true, length: { minimum: 2, maximum: 20 }
+
+  validates_uniqueness_of :username
+  scope :all_except, ->(user) { where.not(id: user) }
+  after_create_commit { broadcast_append_to 'users' }
+
+  has_many :messages
 end
