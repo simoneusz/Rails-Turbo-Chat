99d57a20fc9ac09a52f559db5c87b2ba23549b37
Ilya <simonewarlet@gmail.com>
2025-02-04 11:13:08 +0200

    Add 'add to contacts' button to user info

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,12 +2,20 @@ class ContactsController < ApplicationController
   def index
     @contacts = current_user.contacts
     @pending_contacts = current_user.pending_contacts
+    @users = User.all_except(current_user)
+    render 'rooms/index'
   end
 
   def create
     user = User.find(params[:contact_id])
-    current_user.accept_contact(user)
-    redirect_to contact_path(user), notice: "Request sent to #{user.email}"
+    @users = User.all_except(current_user)
+    # current_user.accept_contact(user)
+    # redirect_to contact_path(user), notice: "Request sent to #{user.email}"
+    if current_user.request_contact(user)
+      render 'rooms/index', notice: 'Requested contact successfully.'
+    else
+      render 'rooms/index', alert: 'Requested contact could not be created.'
+    end
   end
 
   def update
