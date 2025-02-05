e2ce470ea9b7854bcb688daf5682e3a894a8bc1d
Ilya <simonewarlet@gmail.com>
2025-02-05 10:27:23 +0200

    Add boolean methods for status

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -5,4 +5,16 @@ class Contact < ApplicationRecord
   enum status: %i[pending accepted rejected], _prefix: true
 
   validates :user_id, uniqueness: { scope: :contact_id }
+
+  def rejected?
+    status == 'rejected'
+  end
+
+  def accepted?
+    status == 'accepted'
+  end
+
+  def pending?
+    status == 'pending'
+  end
 end
