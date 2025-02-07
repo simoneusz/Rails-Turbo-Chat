a597d50d913bc1c5e6ec45c325d14d53e94cebca
Ilya <simonewarlet@gmail.com>
2025-02-07 16:04:12 +0200

    Add set query method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_query
+
+  def set_query
+    @query = User.ransack(params[:query])
+    @users = []
+  end
 
   protected
 
