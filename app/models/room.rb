7a672a1a12571eb819611291ec817e92204ff47a
Ilya <simonewarlet@gmail.com>
2025-02-11 10:51:57 +0200

    Change user role for p2p chat to peer

--- app/models/room.rb
+++ app/models/room.rb
@@ -13,7 +13,7 @@ class Room < ApplicationRecord
   def self.create_private_room(users, room_name)
     single_room = Room.create(name: room_name, is_private: true)
     users.each do |user|
-      Participant.create(user_id: user.id, room_id: single_room.id, role: :moderator)
+      Participant.create(user_id: user.id, room_id: single_room.id, role: :peer)
     end
     single_room
   end
