cfa8fba3f298893fb45cd77bfe50a9a6610a344c
Ilya <simonewarlet@gmail.com>
2025-02-12 11:34:33 +0200

    Check if user belongs to room through participant

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,12 @@
 class MessagesController < ApplicationController
   def create
-    @current_user = current_user
-    @message = @current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
+    @room = Room.find(params[:room_id])
+    if @room.participant?(current_user)
+      @message = current_user.messages.create(content: msg_params[:content], room_id: params[:room_id])
+    else
+      flash[:alert] = 'You cant send messages here'
+      redirect_to room_path(@room)
+    end
   end
 
   private
