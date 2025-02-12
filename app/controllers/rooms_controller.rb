054aca3eabfd25d9f96314f55485241c24b91ab6
Ilya <simonewarlet@gmail.com>
2025-02-12 10:27:14 +0200

    Add set user method

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
   before_action :authenticate_user!
 
   before_action :set_room, only: %i[add_participant remove_participant block_participant unblock_participant]
+  before_action :set_user, only: %i[add_participant remove_participant block_participant unblock_participant]
   before_action :authorize_room, only: %i[add_participant remove_participant block_participant unblock_participant]
   def index
     @room = Room.new
@@ -35,10 +36,9 @@ class RoomsController < ApplicationController
   end
 
   def add_participant
-    user = User.find(params[:user_id])
     if Pundit.policy(current_user, @room).add_participant?
-      @room.add_participant(current_user, user, :member)
-      flash[:notice] = "#{user.username} was added to the room"
+      @room.add_participant(current_user, @user, :member)
+      flash[:notice] = "#{@user.username} was added to the room"
     else
       flash[:alert] = "You don't have permission to add users"
     end
@@ -47,12 +47,11 @@ class RoomsController < ApplicationController
   end
 
   def remove_participant
-    user = User.find(params[:user_id])
-    participant = @room.participants.find_by(user_id: user.id)
+    participant = @room.participants.find_by(user_id: @user.id)
 
     if participant && Pundit.policy(current_user, @room).remove_participant?
       participant.destroy
-      flash[:notice] = "#{user.username} was removed from the room"
+      flash[:notice] = "#{@user.username} was removed from the room"
     else
       flash[:alert] = "You don't have permission to remove users"
     end
@@ -61,11 +60,10 @@ class RoomsController < ApplicationController
   end
 
   def block_participant
-    user = User.find(params[:user_id])
-    participant = @room.participants.find_by(user_id: user.id)
+    participant = @room.participants.find_by(user_id: @user.id)
     if Pundit.policy(current_user, @room).block_participant?
       participant.update(role: :blocked)
-      flash[:notice] = "#{user.username} was blocked"
+      flash[:notice] = "#{@user.username} was blocked"
     else
       flash[:alert] = "You don't have permission to block users"
     end
@@ -73,11 +71,10 @@ class RoomsController < ApplicationController
   end
 
   def unblock_participant
-    user = User.find(params[:user_id])
-    participant = @room.participants.find_by(user_id: user.id)
+    participant = @room.participants.find_by(user_id: @user.id)
     if Pundit.policy(current_user, @room).block_participant?
       participant.update(role: :member)
-      flash[:notice] = "#{user.username} was unblocked"
+      flash[:notice] = "#{@user.username} was unblocked"
     else
       flash[:alert] = "You don't have permission to unblock users"
     end
@@ -97,6 +94,10 @@ class RoomsController < ApplicationController
             else
               Room.find(params[:id])
             end
+  end
+
+  def set_user
+    @user = User.find(params[:user_id])
   end
 
   def authorize_room
