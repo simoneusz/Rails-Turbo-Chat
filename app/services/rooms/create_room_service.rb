3a18cc51adb23900a1b2f254615f0c850e4f6d5b
Ilya <simonewarlet@gmail.com>
2025-02-19 14:57:08 +0200

    Adjust service to Application Service and returning result as Result

--- app/services/rooms/create_room_service.rb
+++ app/services/rooms/create_room_service.rb
@@ -1,21 +1,35 @@
 # frozen_string_literal: true
 
 module Rooms
-  class CreateRoomService
+  class CreateRoomService < ApplicationService
+    CODE_ROOM_NOT_VALID = :room_not_valid
+
     def initialize(room_params, current_user)
+      super()
       @room_params = room_params
       @current_user = current_user
     end
 
     def call
-      @room = Room.new(@room_params)
-      ActiveRecord::Base.transaction do
-        raise ActiveRecord::Rollback unless @room.save
+      room = create_room
+      return room_not_valid_error(room) unless room.persisted?
 
-        @room.participants.create(user: @current_user, role: owner) if @room.save
-      end
+      add_owner(room)
+      success(room)
+    end
 
-      @room
+    private
+
+    def create_room
+      Room.create(@room_params)
+    end
+
+    def add_owner(room)
+      room.participants.create(user: @current_user, role: :owner)
+    end
+
+    def room_not_valid_error(room)
+      error(room.errors.full_messages.to_sentence, code: CODE_ROOM_NOT_VALID)
     end
   end
 end
