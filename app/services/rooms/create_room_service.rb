870a3811e7714cd9cd3bfaa39d05f3614b0b2177
Ilya <simonewarlet@gmail.com>
2025-02-19 15:26:22 +0200

    Add code error for new invalid participant

--- app/services/rooms/create_room_service.rb
+++ app/services/rooms/create_room_service.rb
@@ -2,7 +2,7 @@
 
 module Rooms
   class CreateRoomService < ApplicationService
-    CODE_ROOM_NOT_VALID = :room_invalid
+    CODE_ROOM_NOT_VALID = :new_room_invalid
 
     def initialize(room_params, current_user)
       super()
