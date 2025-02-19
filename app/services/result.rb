190a99e732429ee9af9b14bcef1a0aea43c952e5
Ilya <simonewarlet@gmail.com>
2025-02-19 14:03:15 +0200

    Add result class for services return values

---
+++ app/services/result.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+class Result
+  attr_accessor :status, :data, :error_code
+
+  def initialize(status, data: nil, error_code: nil)
+    @status = status
+    @data = data
+    @error_code = error_code
+  end
+
+  def success?
+    status
+  end
+
+  def error?
+    !success?
+  end
+end
