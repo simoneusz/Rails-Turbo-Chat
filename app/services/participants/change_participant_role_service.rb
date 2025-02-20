fa37060079431bd0a1d2da28917671c0506379da
Ilya <simonewarlet@gmail.com>
2025-02-20 11:05:09 +0200

    Add change participant service

---
+++ app/services/participants/change_participant_role_service.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module Participants
+  class ChangeParticipantRoleService < ApplicationService
+    CODE_UNKNOWN_ROLE = :unknown_role
+
+    def initialize(participant, new_role)
+      super()
+      @participant = participant
+      @new_role = new_role
+    end
+
+    def call
+      return error_invalid_participant unless @participant
+      return error_unknown_role unless role_exist?(@new_role)
+
+      update_participant_role
+
+      success(@participant)
+    end
+  end
+
+  private
+
+  def update_participant_role
+    @participant.update(role: @new_role)
+  end
+
+  def role_exist?(role)
+    Participant.roles.include?(role)
+  end
+
+  def error_invalid_participant
+    error(code: CODE_PARTICIPANT_DOESNT_EXIST)
+  end
+
+  def error_unknown_role
+    error(code: CODE_UNKNOWN_ROLE)
+  end
+end
