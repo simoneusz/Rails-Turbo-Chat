02e0269634eeb3633f5985a554e7a262ab3086ab
Ilya <simonewarlet@gmail.com>
2025-02-20 10:38:26 +0200

    Refactor service with ApplicationService

--- app/services/participants/remove_participant_service.rb
+++ app/services/participants/remove_participant_service.rb
@@ -1,18 +1,30 @@
+# frozen_string_literal: true
+
 module Participants
-  class RemoveParticipantService
+  class RemoveParticipantService < ApplicationService
+    CODE_PARTICIPANT_DOESNT_EXIST = :participant_doesnt_exist
+
     def initialize(room, user)
+      super()
       @room = room
       @user = user
     end
 
     def call
       participant = @room.find_participant(@user)
+      return error_cant_find_participants(participant) unless participant
 
-      return false unless participant
+      remove_participant(participant)
 
+      success(participant)
+    end
+
+    def remove_participant(participant)
       @room.participants.delete(participant)
+    end
 
-      true
+    def error_cant_find_participants(_participant)
+      error(code: CODE_PARTICIPANT_DOESNT_EXIST)
     end
   end
 end
