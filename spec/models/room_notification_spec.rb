f980c3b43434d6bd9f738512df94233d2f12d2e3
Ilya <simonewarlet@gmail.com>
2025-03-06 16:10:02 +0200

    Fill room notification spec

--- spec/models/room_notification_spec.rb
+++ spec/models/room_notification_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe RoomNotification, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    subject { create(:room_notification) }
+
+    it { should validate_presence_of(:message) }
+  end
 end
