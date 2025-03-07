8d983f82e4ca6f68b4d4ce8bb4692cbb2021809b
Ilya <simonewarlet@gmail.com>
2025-03-07 09:50:10 +0200

    Add UserAvatarUploader thumbnail and use it in edit profile form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,7 +31,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username first_name last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[username first_name last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[username first_name last_name avatar])
   end
 
   private
--- app/uploaders/user_avatar_uploader.rb
+++ app/uploaders/user_avatar_uploader.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class UserAvatarUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model&.id}"
@@ -16,6 +18,10 @@ class UserAvatarUploader < CarrierWave::Uploader::Base
 
   def filename
     "#{secure_token}.#{file&.extension}"
+  end
+
+  version :thumb do
+    process resize_to_fill: [50, 50]
   end
 
   protected
