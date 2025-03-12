bd7e67b630df14f78fcfcf2e9f52df13815b96e5
Ilya <simonewarlet@gmail.com>
2025-03-12 14:14:35 +0200

    Remove notifications on create user

--- app/models/concerns/notificable.rb
+++ app/models/concerns/notificable.rb
@@ -5,14 +5,5 @@ module Notificable
 
   included do
     has_many :notifications, as: :item, dependent: :destroy
-    after_create_commit :send_notifications_to_users
-  end
-
-  def send_notifications_to_users
-    return unless respond_to? :user_ids
-
-    user_ids&.each do |user_id|
-      Notification.create user_id: user_id, item: self
-    end
   end
 end
