107fcc4d3de47038cfcb134882d88e4069836c8d
Ilya <simonewarlet@gmail.com>
2025-03-13 13:13:32 +0200

    Add seeds for creation messages in specific room from all participants

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,21 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-10.times do
-  user = User.new(
-    email: Faker::Internet.unique.email,
-    username: Faker::Internet.username,
-    first_name: Faker::Name.name,
-    last_name: Faker::Name.last_name,
-    password: '123123',
-    password_confirmation: '123123'
-  )
-  p "cant create #{user}  #{user.errors.full_messages}" unless user.save
+# 10.times do
+#   user = User.new(
+#     email: Faker::Internet.unique.email,
+#     username: Faker::Internet.username,
+#     first_name: Faker::Name.name,
+#     last_name: Faker::Name.last_name,
+#     password: '123123',
+#     password_confirmation: '123123'
+#   )
+#   p "cant create #{user}  #{user.errors.full_messages}" unless user.save
+# end
+
+
+room = Room.find(50)
+users = room.participants.map { |participant| participant.user}
+100.times do
+  room.messages.create(user: users.sample, content: Faker::Hipster.sentence)
 end
