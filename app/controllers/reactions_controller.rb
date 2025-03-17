35150adbcffc3310fec810021ce16d2ede65e2aa
Ilya <simonewarlet@gmail.com>
2025-03-17 11:08:01 +0200

    Configure turbo stream for message reactions via broadcasting to all participants

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -1,29 +1,17 @@
 # frozen_string_literal: true
 
 class ReactionsController < ApplicationController
+  before_action :authenticate_user!
+
   def create
     @message = Message.find(params[:message_id])
     @reaction = @message.reactions.find_or_initialize_by(user: current_user, emoji: params[:emoji])
-    @reaction.count += 1
-    @reaction.save
+    @reaction.count = @reaction.count.to_i + 1
+    @reaction.save!
 
     respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("message_#{@message.id}_reactions", partial: 'messages/message_reactions',
-                                                                                      locals: { message: @message })
-      end
+      format.turbo_stream
       format.html { redirect_to room_path(@message.room) }
-    end
-  end
-
-  def destroy
-    message = Message.find(params[:message_id])
-    reaction = message.reactions.find_by(emoji: params[:emoji], user: current_user)
-
-    if reaction.count == 1
-      reaction.destroy
-    else
-      reaction.count -= 1
     end
   end
 end
--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -3,4 +3,16 @@
 class Reaction < ApplicationRecord
   belongs_to :message
   belongs_to :user
+
+  after_create_commit -> { broadcast_replace }
+  after_update_commit -> { broadcast_replace }
+
+  private
+
+  def broadcast_replace
+    broadcast_replace_to "message_#{message.id}_reactions",
+                         target: "reactions_message_#{message.id}",
+                         partial: 'messages/message_reactions',
+                         locals: { message: message }
+  end
 end
