a8457f3e99f94a0f827cf9eccb12989c30cda26a
Ilya <simonewarlet@gmail.com>
2025-03-19 13:17:19 +0200

    Add test for messages replies

--- spec/requests/messages_controller_spec.rb
+++ spec/requests/messages_controller_spec.rb
@@ -28,6 +28,45 @@ RSpec.describe MessagesController, type: :controller do
       end
     end
 
+    context 'when message was replied' do
+      subject(:reply) do
+        post :create, params: { room_id: room.id, message: { content: 'Hello', parent_message_id: message.id } }
+      end
+
+      before do
+        room.participants.create(user: user)
+        reply
+      end
+
+      it 'creates reply with parent_message_id field' do
+        expect(Message.last.parent_message_id).to eq(message.id)
+      end
+
+      it 'creates reply with replied true' do
+        expect(Message.last.replied).to be_truthy
+      end
+    end
+
+    context 'when replied message was deleted' do
+      subject(:reply) do
+        post :create, params: { room_id: room.id, message: { content: 'Hello', parent_message_id: message.id } }
+      end
+
+      before do
+        room.participants.create(user: user)
+        reply
+        room.messages.first.destroy
+      end
+
+      it 'nullifies parent_message_id' do
+        expect(Message.last.parent_message_id).to be_nil
+      end
+
+      it 'leaves replied to true' do
+        expect(Message.last.replied).to be_truthy
+      end
+    end
+
     context 'when user is not a participant' do
       subject(:create_message) { post :create, params: { room_id: room.id, message: { content: 'Hello' } } }
 
