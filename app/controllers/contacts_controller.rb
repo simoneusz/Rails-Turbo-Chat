7c8ff3e9e965aa8487844d14ab87e3c276fe49e5
Ilya <simonewarlet@gmail.com>
2025-03-21 13:42:50 +0200

    Move user definition to set_user callback

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 
 class ContactsController < ApplicationController
+  before_action :set_user, only: %i[update destroy delete]
   def index
     @contacts = current_user.contacts
   end
 
   def create
-    user = User.find(params[:contact_id])
+    @user = User.find(params[:contact_id])
     @users = User.all_except(current_user)
-    if current_user.request_contact(user)
+    if current_user.request_contact(@user)
       redirect_to rooms_path, notice: 'Requested contact successfully.'
     else
       redirect_to rooms_path, alert: 'Requested contact could not be created.'
@@ -16,20 +17,23 @@ class ContactsController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    current_user.accept_contact(user)
-    redirect_to contact_path(user), notice: 'Request accepted'
+    current_user.accept_contact(@user)
+    redirect_to contact_path(@user), notice: 'Request accepted'
   end
 
   def destroy
-    user = User.find(params[:id])
-    current_user.reject_contact(user)
+    current_user.reject_contact(@user)
     redirect_to contacts_path, notice: 'Request destroyed'
   end
 
   def delete
-    user = User.find(params[:id])
-    current_user.delete_contact(user)
+    current_user.delete_contact(@user)
     redirect_to contacts_path, notice: 'Contact deleted'
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
