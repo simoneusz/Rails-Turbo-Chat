535a9a4d7ba3bcc6f59e9aba0462ef8ec6096035
Ilya <simonewarlet@gmail.com>
2025-03-28 10:30:29 +0200

    Redirect to rooms_path after sign in

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -8,7 +8,7 @@ module Users
     # end
 
     def after_sign_out_path_for(_resource_or_scope)
-      new_user_session_path
+      rooms_path
     end
 
     def after_sign_in_path_for(resource_or_scope)
