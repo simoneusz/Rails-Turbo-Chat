2ea543339126eca27524a588a2448f8003c23106
Ilya <simonewarlet@gmail.com>
2025-04-03 12:10:22 +0300

    Remove comments

--- app/javascript/channels/appearances_channel.js
+++ app/javascript/channels/appearances_channel.js
@@ -6,47 +6,36 @@ let timer = 0;
 consumer.subscriptions.create("AppearancesChannel", {
   initialized() {},
   connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("Connected");
     resetFunc = () => this.resetTimer(this.uninstall);
     this.install();
     window.addEventListener("turbo:load", () => this.resetTimer());
   },
 
   disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log("Connected");
     this.uninstall();
   },
   rejected() {
-    console.log("Rejected");
     this.uninstall();
   },
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
   },
   online() {
-    console.log("online");
     this.perform("online");
   },
   away() {
-    console.log("away");
     this.perform("away");
   },
   offline() {
-    console.log("offline");
     this.perform("offline");
   },
   uninstall() {
     const shouldRun = document.getElementById("appearances_channel");
-    console.log(shouldRun)
     if (!shouldRun) {
       clearTimeout(timer);
       this.perform("offline");
     }
   },
   install() {
-    console.log("Install");
     window.removeEventListener("load", resetFunc);
     window.removeEventListener("DOMContentLoaded", resetFunc);
     window.removeEventListener("click", resetFunc);
