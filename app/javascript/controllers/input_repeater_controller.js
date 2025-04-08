53d718f7a155ded4a9754c3e752aa9ec5f400f34
Ilya <simonewarlet@gmail.com>
2025-04-08 15:02:23 +0300

    Add stimulus controller for repeating text for new room form preview

---
+++ app/javascript/controllers/input_repeater_controller.js
@@ -0,0 +1,16 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="input-repeater"
+export default class extends Controller {
+  static targets = ["inputRepeater", "outputRepeater"]
+  connect() {}
+
+  repeat(){
+    let value = this.inputRepeaterTarget.value;
+    if (value === "") {
+      this.outputRepeaterTarget.innerHTML = "room name"
+    } else{
+      this.outputRepeaterTarget.innerHTML = value;
+    }
+  }
+}
