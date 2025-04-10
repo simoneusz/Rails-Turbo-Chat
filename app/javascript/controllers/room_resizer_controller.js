5cba2e68acfb417154fe4443eb1e64ef9b2accfa
Ilya <simonewarlet@gmail.com>
2025-04-10 14:34:48 +0300

    Remov previous gutter until it's not initialized by Split

--- app/javascript/controllers/room_resizer_controller.js
+++ app/javascript/controllers/room_resizer_controller.js
@@ -4,12 +4,16 @@ import Split from "split.js";
 // Connects to data-controller="room-resizer"
 export default class extends Controller {
   connect() {
+    // Removing previous gutter until it's not initialized by Split
+    let gutter = document.querySelector(".gutter")
+    if (gutter) gutter.remove();
+
     let main = document.querySelector("main")
     let elements = main.childNodes;
     Split([...main.children], {
       sizes: [25, 75],
       minSize: [250, 500],
     })
-    let gutter = document.querySelector(".gutter")
+    gutter = document.querySelector(".gutter")
   }
 }
