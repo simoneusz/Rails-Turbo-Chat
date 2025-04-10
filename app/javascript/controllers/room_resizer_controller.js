41a568123406d659a169f4b18ffd8aa5309f32b8
Ilya <simonewarlet@gmail.com>
2025-04-10 14:37:33 +0300

    Move gutter removing to disconnect

--- app/javascript/controllers/room_resizer_controller.js
+++ app/javascript/controllers/room_resizer_controller.js
@@ -4,9 +4,6 @@ import Split from "split.js";
 // Connects to data-controller="room-resizer"
 export default class extends Controller {
   connect() {
-    // Removing previous gutter until it's not initialized by Split
-    let gutter = document.querySelector(".gutter")
-    if (gutter) gutter.remove();
 
     let main = document.querySelector("main")
     let elements = main.childNodes;
@@ -14,6 +11,11 @@ export default class extends Controller {
       sizes: [25, 75],
       minSize: [250, 500],
     })
-    gutter = document.querySelector(".gutter")
+  }
+
+  disconnect() {
+    // Removing previous gutter until it's not initialized by Split
+    let gutter = document.querySelector(".gutter")
+    if (gutter) gutter.remove();
   }
 }
