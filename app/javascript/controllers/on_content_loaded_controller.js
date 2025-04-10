1b63f1b1865951de0f901c9df388455a1f9dbc26
Ilya <simonewarlet@gmail.com>
2025-04-10 11:31:57 +0300

    Update vendor files

---
+++ app/javascript/controllers/on_content_loaded_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+import * as bootstrap from "bootstrap";
+
+// Connects to data-controller="on-content-loaded"
+export default class extends Controller {
+  connect() {
+    function searchHotkey(e) {
+      e.preventDefault()
+      if (e.ctrlKey && e.code === 'KeyM') {
+        let modal = document.querySelector("#searchModal");
+        if (!modal.classList.contains("show")) {
+          let b_modal = new bootstrap.Modal(modal)
+          b_modal.show()
+        }
+      }
+    }
+
+    document.addEventListener('keyup', searchHotkey, false);
+  }
+}
