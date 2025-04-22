ffebde3987f3bf10c143e61b2213edf07807bc75
Ilya <simonewarlet@gmail.com>
2025-04-22 11:13:40 +0300

    Updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_04_07_124104) do
+ActiveRecord::Schema[7.1].define(version: 2025_04_22_071157) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -205,6 +205,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_04_07_124104) do
     t.string "unconfirmed_email"
     t.integer "status", default: 0
     t.boolean "status_changed", default: false
+    t.string "display_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["username"], name: "index_users_on_username", unique: true
